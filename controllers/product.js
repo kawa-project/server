62c2853c536634c8336d6fff721dee02aabae46d
Danang Putra Bahari <danangbahari89@gmail.com>
2019-12-27 09:11:24 +0700

    Update product 1. product controllers 2. create routing product for admin and customer 3. create model with 2 shcema, for main product and attributes schema contains => a. color, b. size, c. stock

---
+++ controllers/product.js
@@ -0,0 +1,202 @@
+const { Product } = require('../models');
+
+class ProductController {
+    static createProduct(req, res, next) {
+        const { name, price, image, color, size, stock } = req.body;
+        let attributeData = {
+            color: color.toLowerCase(),
+            size: size,
+            stock
+        };
+        Product.create({
+            name,
+            price,
+            image,
+            attributes: attributeData
+        })
+            .then(product => {
+                res.status(201).json(product);
+            })
+            .catch(next);
+    }
+
+    static getOneProduct(req, res, next) {
+        const { id } = req.params;
+        Product.findOne({
+            _id: id
+        })
+            .then(product => {
+                if (product) {
+                    res.status(200).json(product);
+                } else {
+                    throw {
+                        status: 404,
+                        message: 'There is no product with that id'
+                    };
+                }
+            })
+            .catch(next);
+    }
+
+    static updateProduct(req, res, next) {
+        const { id } = req.body;
+        Product.findOne({
+            _id: id
+        })
+            .then(product => {
+                if (product) {
+                    let input = req.body;
+                    let editData = {};
+                    for (const data in input) {
+                        editData[data] = input[data];
+                    }
+                    return Product.findOneAndUpdate(
+                        {
+                            _id: product._id
+                        },
+                        editData,
+                        { new: true }
+                    );
+                } else {
+                    throw {
+                        status: 400,
+                        message: 'There is no product with that id'
+                    };
+                }
+            })
+            .then(newProduct => {
+                res.status(200).json(newProduct);
+            })
+            .catch(next);
+    }
+
+    static updateAttributes(req, res, next) {
+        const { color, size, stock } = req.body;
+        const { id } = req.params;
+        Product.findOne({
+            _id: id
+        })
+            .then(product => {
+                let attribute = product.attributes;
+                let isExist = false;
+                attribute.forEach(data => {
+                    if (data.color === color && data.size === Number(size)) {
+                        data.stock += Number(stock);
+                        isExist = true;
+                    }
+                });
+                if (isExist) {
+                    product.save();
+                    res.status(200).json(product);
+                } else {
+                    return Product.findOneAndUpdate(
+                        {
+                            _id: product._id
+                        },
+                        {
+                            $push: {
+                                attributes: {
+                                    color,
+                                    size: Number(size),
+                                    stock: Number(stock)
+                                }
+                            }
+                        },
+                        { new: true }
+                    );
+                }
+            })
+            .then(newProduct => {
+                res.status(200).json(newProduct);
+            })
+            .catch(next);
+    }
+
+    static getDetailAttribute(req, res, next) {
+        const { color } = req.query;
+        const { id } = req.params;
+        Product.findOne({
+            _id: id
+        })
+            .then(product => {
+                let attribute = product.attributes;
+                let arr = [];
+                attribute.forEach(data => {
+                    if (data.color === color) {
+                        arr.push(data);
+                    }
+                });
+                res.status(200).json(arr);
+            })
+            .catch(next);
+    }
+
+    static updateStock(req, res, next) {
+        const { color, size, stock } = req.query;
+        const { id } = req.params;
+        Product.findOne({
+            _id: id
+        })
+            .then(product => {
+                if (product) {
+                    let attribute = product.attributes;
+                    let isExist = false;
+                    attribute.forEach(data => {
+                        if (
+                            data.color === color &&
+                            data.size === Number(size)
+                        ) {
+                            if (data.stock >= Number(stock)) {
+                                data.stock -= Number(stock);
+                                isExist = true;
+                            } else {
+                                throw {
+                                    status: 400,
+                                    message: 'Insufficient Stock'
+                                };
+                            }
+                        }
+                    });
+                    if (isExist) {
+                        product.save();
+                        res.status(200).json({
+                            message: 'Update stock success'
+                        });
+                    } else {
+                        throw {
+                            status: 404,
+                            message: 'There is no attribute match'
+                        };
+                    }
+                } else {
+                    throw {
+                        status: 404,
+                        message: 'There is no product with that id'
+                    };
+                }
+            })
+            .catch(next);
+    }
+
+    static deleteProduct(req, res, next) {
+        const { id } = req.params;
+        Product.deleteOne({
+            _id: id
+        })
+            .then(result => {
+                if (result.deletedCount > 0) {
+                    res.status(200).json({
+                        message: 'Success delete product'
+                    });
+                } else {
+                    throw {
+                        status: 404,
+                        message: 'There is no product with that id'
+                    };
+                }
+            })
+            .catch(next);
+    }
+}
+
+module.exports = ProductController;
